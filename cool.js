74dd24146944a470d1316e7017522144f48fe275
kanake10 <10kanake@gmail.com>
2021-01-28 21:39:29 +0300

    js

--- cool.js
+++ cool.js
@@ -23,3 +23,21 @@ function showSlides() {
   setTimeout(showSlides, 3500);
    // Change image every 3.5 seconds
 }
+
+window.addEventListener("load", function() {
+  var elements = document.getElementsByClassName("rainbowText");
+  for (let i = 0; i < elements.length; i++) {
+    generateRainbowText(elements[i]);
+  }
+});
+
+function generateRainbowText(element) {
+  var text = element.innerText;
+  element.innerHTML = "";
+  for (let i = 0; i < text.length; i++) {
+    let charElem = document.createElement("span");
+    charElem.style.color = "hsl(" + (360 * i / text.length) + ",80%,50%)";
+    charElem.innerHTML = text[i];
+    element.appendChild(charElem);
+  }
+}
